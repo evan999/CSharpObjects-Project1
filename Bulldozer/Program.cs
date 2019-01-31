1f9dd39fc107fb1cd1f913da0c8e67c85b75e221
evan999 <emeshberge@gmail.com>
2019-01-30 22:35:15 -0500

    Instantiate BullDozer class

--- Bulldozer/Program.cs
+++ Bulldozer/Program.cs
@@ -8,8 +8,30 @@ namespace Bulldozer
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var dozer = new BullDozer("Yellow", "Mini");
+            dozer.color = "Yellow";
+            dozer.size = "Mini";
+
+            var dozer2 = new BullDozer("Red", "Large");
+
+            Console.WriteLine("Bulldozer 1 is " + dozer.color);
+            Console.WriteLine("Bulldozer 2 is " + dozer.color);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
+    }
+
+    public class BullDozer
+    {
+        public BullDozer (string _color, string _size)
+        {
+            this.color = _color;
+            this.size = _size;
+        }
+
+        public string color { get; set; }
+        public string size { get; set; }
     }
 }
